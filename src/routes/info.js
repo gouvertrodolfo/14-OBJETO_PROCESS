5f92b1020820e2e4ed077ac6616ddf40afea11f5
r02 <rgouvert@gmail.com.ar>
2022-01-02 20:04:29 -0300

    padme

--- src/routes/info.js
+++ src/routes/info.js
@@ -6,17 +6,17 @@ const info = Router();
 
 info.get('/', (req, res) => {
 
-    
+
     const info = {
-         'Argumentos_Entrada': yargs,
+        'Argumentos_Entrada': yargs,
         'Path': process.execPath,
         'SO:': process.platform,
         'id_proceso ': process.pid,
         'Node_version': process.version,
         'Carpeta_Proyecto': process.cwd(),
-         'Memoria': process.memoryUsage().rss
+        'Memoria': process.memoryUsage().rss
     }
-console.log(info)
+    console.log(info)
     res.json(info)
 });
 
