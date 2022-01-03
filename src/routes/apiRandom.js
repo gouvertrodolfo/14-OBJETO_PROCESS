9caefdff8b70917c11a2b0c5c7b82328f5afef13
r02 <rgouvert@gmail.com.ar>
2022-01-02 21:12:02 -0300

    padme

--- src/routes/apiRandom.js
+++ src/routes/apiRandom.js
@@ -1,9 +1,43 @@
 const { Router } = require('express');
 
 const apiRandom = Router();
+const numeros = new Map()
 
-apiRandom.get('/:cant', (req, res) => {
-    
+function getRandomInt() {
+    return Math.floor(Math.random() * 10000);
+}
+
+function generateRandom(req, res, next) {
+    const { cant } = req.params
+
+    let i = 0
+    let number
+    let valor
+
+    while (i < cant) {
+        number = getRandomInt();
+        valor = numeros.get(number)
+
+        if (valor === undefined) { numeros.set(number, 0) }
+        else {
+
+            valor = valor + 1
+            numeros.set(number, valor++)
+        }
+        i++;
+    }
+
+
+    next()
+}
+
+apiRandom.get('/:cant', generateRandom, (req, res) => {
+
+    let resultado = []
+    for (const [numero, cantOcu] of numeros) {
+        resultado.push({ 'numero': numero, 'ocurrencias': cantOcu })
+    }
+    res.json(resultado);
 });
 
 exports.apiRandom = apiRandom;
